5a2e2e26b09568ba3375a30ccc7f705400671584
mikalikahi <sweeney.phil@gmail.com>
2022-11-15 10:15:37 +0800

    Savings account model with relations and notes for queries from database

---
+++ src/expenses_tracker/models/SavingsAccountModel.java
@@ -0,0 +1,33 @@
+package expenses_tracker.models;
+
+import java.math.BigDecimal;
+
+public class SavingsAccountModel {
+
+    int id;
+
+    int bankId;
+    BigDecimal accountBalance;
+
+    int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public SavingsAccountModel(int id, int userId, int bankId) {
+        this.id = id;
+        this.userId = userId;
+        this.bankId = bankId;
+        this.accountBalance = BigDecimal.valueOf(00.00);
+    }
+
+
+
+
+
+}
